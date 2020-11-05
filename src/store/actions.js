03b68b114cafa5bbaac0449e5df56aac7ddc1519
Eray Kısabacak <nefret19981998@hotmail.com>
2020-11-05 14:10:30 +0300

    Websocket Adress Change

--- src/store/actions.js
+++ src/store/actions.js
@@ -33,7 +33,7 @@ export const getAPI = ({ commit }) => {
 
 export const getAPIWebSocket = async ({ commit }) => {
   let connection;
-  connection = await new WebSocket('ws://localhost:3000');
+  connection = await new WebSocket('ws://dovizapi.herokuapp.com/');
 
   var msg;
 
